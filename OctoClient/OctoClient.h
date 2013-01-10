8f04b2f63433a3eefa739fb489b22a36817d9002
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-01-09 16:11:51 -0800

    Add a framework header and some organization

---
+++ OctoClient/OctoClient.h
@@ -0,0 +1,34 @@
+//
+//  OctoClient.h
+//  OctoClient
+//
+//  Created by Justin Spahr-Summers on 2013-01-09.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import <OctoClient/OCTClient.h>
+#import <OctoClient/OCTCommitComment.h>
+#import <OctoClient/OCTCommitCommentEvent.h>
+#import <OctoClient/OCTEntity.h>
+#import <OctoClient/OCTEvent.h>
+#import <OctoClient/OCTIssue.h>
+#import <OctoClient/OCTIssueComment.h>
+#import <OctoClient/OCTIssueCommentEvent.h>
+#import <OctoClient/OCTIssueEvent.h>
+#import <OctoClient/OCTObject+Private.h>
+#import <OctoClient/OCTObject.h>
+#import <OctoClient/OCTOrg.h>
+#import <OctoClient/OCTPlan.h>
+#import <OctoClient/OCTPublicKey.h>
+#import <OctoClient/OCTPullRequest.h>
+#import <OctoClient/OCTPullRequestComment.h>
+#import <OctoClient/OCTPullRequestCommentEvent.h>
+#import <OctoClient/OCTPullRequestEvent.h>
+#import <OctoClient/OCTPushEvent.h>
+#import <OctoClient/OCTRefEvent.h>
+#import <OctoClient/OCTRepository.h>
+#import <OctoClient/OCTResponse.h>
+#import <OctoClient/OCTServer+Private.h>
+#import <OctoClient/OCTServer.h>
+#import <OctoClient/OCTTeam.h>
+#import <OctoClient/OCTUser.h>
