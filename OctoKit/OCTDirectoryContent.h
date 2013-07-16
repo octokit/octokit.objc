4491b8b434bf56b9ec3008aae2c2f3564443faa4
Aron Cedercrantz <aron@cedercrantz.se>
2013-07-16 17:04:21 +0200

    Remove comment on JSON structure.
    - Not sure how I missed that while looking through the code… :)
    
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

--- OctoKit/OCTDirectoryContent.h
+++ OctoKit/OCTDirectoryContent.h
@@ -8,22 +8,6 @@
 
 #import "OCTContent.h"
 
-//{
-//    "type": "dir",
-//    "size": 0,
-//    "name": "octokit",
-//    "path": "lib/octokit",
-//    "sha": "a84d88e7554fc1fa21bcbc4efae3c782a70d2b9d",
-//    "url": "https://api.github.com/repos/pengwynn/octokit/contents/lib/octokit",
-//    "git_url": "https://api.github.com/repos/pengwynn/octokit/git/trees/a84d88e7554fc1fa21bcbc4efae3c782a70d2b9d",
-//    "html_url": "https://github.com/pengwynn/octokit/tree/master/lib/octokit",
-//    "_links": {
-//      "self": "https://api.github.com/repos/pengwynn/octokit/contents/lib/octokit",
-//      "git": "https://api.github.com/repos/pengwynn/octokit/git/trees/a84d88e7554fc1fa21bcbc4efae3c782a70d2b9d",
-//      "html": "https://github.com/pengwynn/octokit/tree/master/lib/octokit"
-//    }
-//  }
-
 // A directory in a git repository.
 @interface OCTDirectoryContent : OCTContent
 @end
