ef04cd837e0153a69c6e1c9fb4f44d5a2dcbf43f
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-07-31 15:12:53 -0700

    Expose creation method

--- OctoKit/OCTClient.h
+++ OctoKit/OCTClient.h
@@ -454,11 +454,19 @@ typedef enum : NSUInteger {
 
 // Edits one or more files within a gist.
 //
-// edit - The changes to make to the gist.
+// edit - The changes to make to the gist. This must not be nil.
 // gist - The gist to modify. This must not be nil.
 //
 // Returns a signal which will the updated OCTGist and complete. If the client
 // is not `authenticated`, the signal will error immediately.
 - (RACSignal *)applyEdit:(OCTGistEdit *)edit toGist:(OCTGist *)gist;
+
+// Creates a gist using the given changes.
+//
+// edit - The changes to use for creating the gist. This must not be nil.
+//
+// Returns a signal which will the created OCTGist and complete. If the client
+// is not `authenticated`, the signal will error immediately.
+- (RACSignal *)createGistWithEdit:(OCTGistEdit *)edit;
 
 @end
