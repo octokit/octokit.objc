b1f01d401160cc0641ea48e872383d427e79e1ec
Piet Brauer <piet.brauer@xing.com>
2014-02-08 22:14:45 +0100

    Add documentation

--- OctoKit/OCTBranch.h
+++ OctoKit/OCTBranch.h
@@ -11,10 +11,13 @@
 // A GithHub repository branch.
 @interface OCTBranch : OCTObject
 
+// The name of the branch.
 @property (nonatomic, copy, readonly) NSString *name;
 
+// The SHA of the last commit on this branch.
 @property (nonatomic, copy, readonly) NSString *lastCommitSHA;
 
+// The API URL to the last commit on this branch.
 @property (nonatomic, copy, readonly) NSString *lastCommitURL;
 
 @end
