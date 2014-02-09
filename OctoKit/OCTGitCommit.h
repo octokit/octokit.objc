8abb72b86f160f39733233e64d664b647c113d7e
Piet Brauer <piet.brauer@xing.com>
2014-02-09 15:04:40 +0100

    Add docs for OCTGitCommit

--- OctoKit/OCTGitCommit.h
+++ OctoKit/OCTGitCommit.h
@@ -8,16 +8,22 @@
 
 #import <OctoKit/OctoKit.h>
 
+// A git commit.
 @interface OCTGitCommit : OCTObject
 
+// The commit URL for this commit.
 @property (nonatomic, copy, readonly) NSURL *commitURL;
 
+// The commit message for this commit.
 @property (nonatomic, copy, readonly) NSString *message;
 
+// The SHA for this commit.
 @property (nonatomic, copy, readonly) NSString *SHA;
 
+// The committer of this commit.
 @property (nonatomic, copy, readonly) OCTUser *committer;
 
+// The author of this commit.
 @property (nonatomic, copy, readonly) OCTUser *author;
 
 @end
