2264f2dabf4d58cd9090962388e48b2b7bf24953
Piet Brauer <piet.brauer@xing.com>
2014-02-11 20:33:23 +0100

    Modify OCTGitCommitSpec to test full commit and small commit

--- OctoKitTests/OCTGitCommitSpec.m
+++ OctoKitTests/OCTGitCommitSpec.m
@@ -12,33 +12,77 @@
 SpecBegin(OCTGitCommit)
 
 describe(@"github.com git commit", ^{
-	__block NSDictionary *representation;
+	describe(@"parsing a small commit", ^{
+		__block NSDictionary *representation;
 
-	beforeAll(^{
-		NSURL *testDataURL = [[NSBundle bundleForClass:self.class] URLForResource:@"git_commit" withExtension:@"json"];
-		expect(testDataURL).notTo.beNil();
+		beforeAll(^{
+			NSURL *testDataURL = [[NSBundle bundleForClass:self.class] URLForResource:@"git_commit" withExtension:@"json"];
+			expect(testDataURL).notTo.beNil();
 
-		NSData *testContentData = [NSData dataWithContentsOfURL:testDataURL];
-		expect(testContentData).notTo.beNil();
+			NSData *testContentData = [NSData dataWithContentsOfURL:testDataURL];
+			expect(testContentData).notTo.beNil();
 
-		representation = [NSJSONSerialization JSONObjectWithData:testContentData options:0 error:NULL];
-		expect(representation).to.beKindOf(NSDictionary.class);
+			representation = [NSJSONSerialization JSONObjectWithData:testContentData options:0 error:NULL];
+			expect(representation).to.beKindOf(NSDictionary.class);
+		});
+
+		__block OCTGitCommit *commit;
+
+		beforeEach(^{
+			commit = [MTLJSONAdapter modelOfClass:OCTGitCommit.class fromJSONDictionary:representation error:NULL];
+			expect(commit).notTo.beNil();
+		});
+
+		it(@"should initialize from an external representation", ^{
+			expect(commit.commitURL).to.equal([NSURL URLWithString:@"https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e"]);
+			expect(commit.message).to.equal(@"Fix all the bugs");
+			expect(commit.SHA).to.equal(@"6dcb09b5b57875f334f61aebed695e2e4193db5e");
+			expect(commit.committer.login).to.equal(@"octocat");
+			expect(commit.author.login).to.equal(@"octocat");
+			expect(commit.commitDate).to.equal([NSDate dateWithTimeIntervalSince1970:0]);
+			expect(commit.totalChanges).to.beNil();
+			expect(commit.additions).to.beNil();
+			expect(commit.deletions).to.beNil();
+			expect(commit.files).to.beNil();
+		});
 	});
 
-	__block OCTGitCommit *commit;
+	describe(@"parsing a full commit", ^{
+		__block NSDictionary *representation;
 
-	beforeEach(^{
-		commit = [MTLJSONAdapter modelOfClass:OCTGitCommit.class fromJSONDictionary:representation error:NULL];
-		expect(commit).notTo.beNil();
-	});
+		beforeAll(^{
+			NSURL *testDataURL = [[NSBundle bundleForClass:self.class] URLForResource:@"git_commit_full" withExtension:@"json"];
+			expect(testDataURL).notTo.beNil();
 
-	it(@"should initialize from an external representation", ^{
-		expect(commit.commitURL).to.equal([NSURL URLWithString:@"https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e"]);
-		expect(commit.message).to.equal(@"Fix all the bugs");
-		expect(commit.SHA).to.equal(@"6dcb09b5b57875f334f61aebed695e2e4193db5e");
-		expect(commit.committer.login).to.equal(@"octocat");
-		expect(commit.author.login).to.equal(@"octocat");
-		expect(commit.commitDate).to.equal([NSDate dateWithTimeIntervalSince1970:0]);
+			NSData *testContentData = [NSData dataWithContentsOfURL:testDataURL];
+			expect(testContentData).notTo.beNil();
+
+			representation = [NSJSONSerialization JSONObjectWithData:testContentData options:0 error:NULL];
+			expect(representation).to.beKindOf(NSDictionary.class);
+		});
+
+		__block OCTGitCommit *commit;
+
+		beforeEach(^{
+			commit = [MTLJSONAdapter modelOfClass:OCTGitCommit.class fromJSONDictionary:representation error:NULL];
+			expect(commit).notTo.beNil();
+		});
+
+		it(@"should initialize from an external representation", ^{
+			expect(commit.commitURL).to.equal([NSURL URLWithString:@"https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e"]);
+			expect(commit.message).to.equal(@"Fix all the bugs");
+			expect(commit.SHA).to.equal(@"6dcb09b5b57875f334f61aebed695e2e4193db5e");
+			expect(commit.committer.login).to.equal(@"octocat");
+			expect(commit.author.login).to.equal(@"octocat");
+			expect(commit.commitDate).to.equal([NSDate dateWithTimeIntervalSince1970:0]);
+			expect(commit.totalChanges).to.equal(@108);
+			expect(commit.additions).to.equal(@104);
+			expect(commit.deletions).to.equal(@4);
+			expect(commit.files.count).to.equal(1);
+
+			OCTFile *file = commit.files[0];
+			expect(file.class).to.equal(OCTFile.class);
+		});
 	});
 });
 
