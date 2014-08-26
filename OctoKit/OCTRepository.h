51cea4ec3a55ee3dcd78e79e01686f9b0d5a3aa1
Barrett Clark <barrett@websitesbybarrett.com>
2014-08-26 14:13:56 -0500

    Add language from repository response to parsed OCTRepository object

--- OctoKit/OCTRepository.h
+++ OctoKit/OCTRepository.h
@@ -26,6 +26,9 @@
 // The description of this repository.
 @property (nonatomic, copy, readonly) NSString *repoDescription;
 
+// The language of this repository.
+@property (nonatomic, copy, readonly) NSString *language;
+
 // Whether this repository is private to the owner.
 @property (nonatomic, assign, getter = isPrivate, readonly) BOOL private;
 
