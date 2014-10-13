d06acd3c5943469d3d75fa3acf52d6f797c7fa92
joshaber <joshaber@gmail.com>
2014-10-13 16:21:28 -0400

    Test it.

--- OctoKitTests/OCTRepositorySpec.m
+++ OctoKitTests/OCTRepositorySpec.m
@@ -49,6 +49,66 @@ describe(@"from JSON", ^{
 		@"created_at": @"2011-01-26T19:01:12Z",
 		@"updated_at": @"2011-01-26T19:14:43Z",
 		@"default_branch": @"master",
+		@"source": @{
+			@"url": @"https://api.github.com/repos/octocat/Hello-World",
+			@"html_url": @"https://github.com/octocat/Hello-World",
+			@"clone_url": @"https://github.com/octocat/Hello-World.git",
+			@"git_url": @"git://github.com/octocat/Hello-World.git",
+			@"ssh_url": @"git@github.com:octocat/Hello-World.git",
+			@"svn_url": @"https://svn.github.com/octocat/Hello-World",
+			@"mirror_url": @"git://git.example.com/octocat/Hello-World",
+			@"id": @1296268,
+			@"owner": @{
+				@"login": @"octocat",
+			},
+
+			@"name": @"Hello-World",
+			@"full_name": @"octocat/Hello-World",
+			@"description": @"This your first repo!",
+			@"homepage": @"https://github.com",
+			@"language": NSNull.null,
+			@"private": @NO,
+			@"fork": @NO,
+			@"forks": @9,
+			@"watchers": @80,
+			@"size": @108,
+			@"master_branch": @"master",
+			@"open_issues": @0,
+			@"pushed_at": @"2011-01-26T19:06:43Z",
+			@"created_at": @"2011-01-26T19:01:12Z",
+			@"updated_at": @"2011-01-26T19:14:43Z",
+			@"default_branch": @"master",
+		},
+		@"parent": @{
+			@"url": @"https://api.github.com/repos/octocat/Hello-World",
+			@"html_url": @"https://github.com/octocat/Hello-World",
+			@"clone_url": @"https://github.com/octocat/Hello-World.git",
+			@"git_url": @"git://github.com/octocat/Hello-World.git",
+			@"ssh_url": @"git@github.com:octocat/Hello-World.git",
+			@"svn_url": @"https://svn.github.com/octocat/Hello-World",
+			@"mirror_url": @"git://git.example.com/octocat/Hello-World",
+			@"id": @1296267,
+			@"owner": @{
+				@"login": @"octocat",
+			},
+
+			@"name": @"Hello-World",
+			@"full_name": @"octocat/Hello-World",
+			@"description": @"This your first repo!",
+			@"homepage": @"https://github.com",
+			@"language": NSNull.null,
+			@"private": @NO,
+			@"fork": @NO,
+			@"forks": @9,
+			@"watchers": @80,
+			@"size": @108,
+			@"master_branch": @"master",
+			@"open_issues": @0,
+			@"pushed_at": @"2011-01-26T19:06:43Z",
+			@"created_at": @"2011-01-26T19:01:12Z",
+			@"updated_at": @"2011-01-26T19:14:43Z",
+			@"default_branch": @"master",
+		},
 	};
 
 	__block OCTRepository *repository;
@@ -78,6 +138,12 @@ describe(@"from JSON", ^{
 		expect(repository.HTMLURL).to.equal([NSURL URLWithString:@"https://github.com/octocat/Hello-World"]);
 		expect(repository.SSHURL).to.equal(@"git@github.com:octocat/Hello-World.git");
 		expect(repository.defaultBranch).to.equal(@"master");
+
+		expect(repository.forkSource).notTo.beNil();
+		expect(repository.forkSource.objectID).to.equal(@"1296268");
+
+		expect(repository.forkParent).notTo.beNil();
+		expect(repository.forkParent.objectID).to.equal(@"1296267");
 	});
 });
 
