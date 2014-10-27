346077392c1714eaeda2c799bcfc70792b127890
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-27 11:42:05 -0700

    More configuration fixes

--- OctoKit/OCTClient.h
+++ OctoKit/OCTClient.h
@@ -8,6 +8,13 @@
 
 #import <Foundation/Foundation.h>
 #import <SystemConfiguration/SystemConfiguration.h>
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED
+	#import <MobileCoreServices/MobileCoreServices.h>
+#else
+	#import <CoreServices/CoreServices.h>
+#endif
+
 #import <AFNetworking/AFNetworking.h>
 
 @class OCTServer;
