7fcd3e596fbeabc0c6388c9f457b267adbb664a5
leichunfeng <307213080@qq.com>
2015-03-13 22:42:31 +0800

    Use triple /s replace double /s.

--- OctoKit/OCTClient+Issues.h
+++ OctoKit/OCTClient+Issues.h
@@ -10,22 +10,22 @@
 
 @interface OCTClient (Issues)
 
-// Creates an issue.
-//
-// title      - The title of the issue. This must not be nil.
-// body       - The contents of the issue. This can be nil.
-// assignee   - Login for the user that this issue should be assigned to. NOTE:
-//              Only users with push access can set the assignee for new issues.
-//              The assignee is silently dropped otherwise. This can be nil.
-// milestone  - Milestone to associate this issue with. NOTE: Only users with
-//              push access can set the milestone for new issues. The milestone
-//              is silently dropped otherwise. This can be nil.
-// labels     - Labels to associate with this issue. NOTE: Only users with push
-//              access can set labels for new issues. Labels are silently dropped
-//              otherwise. This can be nil.
-// repository - The repository in which to create the issue. This must not be nil.
-//
-// Returns a signal which will send the created `OCTIssue` then complete, or error.
+/// Creates an issue.
+///
+/// title      - The title of the issue. This must not be nil.
+/// body       - The contents of the issue. This can be nil.
+/// assignee   - Login for the user that this issue should be assigned to. NOTE:
+///              Only users with push access can set the assignee for new issues.
+//               The assignee is silently dropped otherwise. This can be nil.
+/// milestone  - Milestone to associate this issue with. NOTE: Only users with
+///              push access can set the milestone for new issues. The milestone
+///              is silently dropped otherwise. This can be nil.
+/// labels     - Labels to associate with this issue. NOTE: Only users with push
+///              access can set labels for new issues. Labels are silently dropped
+///              otherwise. This can be nil.
+/// repository - The repository in which to create the issue. This must not be nil.
+///
+/// Returns a signal which will send the created `OCTIssue` then complete, or error.
 - (RACSignal *)createIssueWithTitle:(NSString *)title body:(NSString *)body assignee:(NSString *)assignee milestone:(NSUInteger)milestone labels:(NSArray *)labels inRepository:(OCTRepository *)repository;
 
 @end
