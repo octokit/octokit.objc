74646075e465d3b810db0c84fa6e20833eb6e79e
leichunfeng <307213080@qq.com>
2015-07-12 19:52:52 +0800

    Fetch user's events.

--- OctoKit/OCTClient+Events.m
+++ OctoKit/OCTClient+Events.m
@@ -11,6 +11,7 @@
 #import "OCTEvent.h"
 #import "OCTUser.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
+#import "RACSignal+OCTClientAdditions.h"
 
 @implementation OCTClient (Events)
 
@@ -20,6 +21,27 @@
 	NSURLRequest *request = [self requestWithMethod:@"GET" path:[NSString stringWithFormat:@"users/%@/received_events", self.user.login] parameters:nil notMatchingEtag:etag];
 	
 	return [self enqueueRequest:request resultClass:OCTEvent.class fetchAllPages:NO];
+}
+
+- (RACSignal *)fetchEventsForUser:(OCTUser *)user page:(NSUInteger)page perPage:(NSUInteger)perPage {
+	NSParameterAssert(user != nil);
+	
+	NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+	
+	if (page >= 1) {
+		parameters[@"page"] = @(page);
+	}
+	
+	if (perPage >= 1 && perPage <= 100) {
+		parameters[@"per_page"] = @(perPage);
+	} else {
+		parameters[@"per_page"] = @(30);
+	}
+	
+	NSString *path = [NSString stringWithFormat:@"users/%@/received_events", user.login];
+	NSMutableURLRequest *request = [self requestWithMethod:@"GET" path:path parameters:parameters notMatchingEtag:nil];
+	
+	return [[self enqueueRequest:request resultClass:OCTEvent.class fetchAllPages:NO] oct_parsedResults];
 }
 
 @end
